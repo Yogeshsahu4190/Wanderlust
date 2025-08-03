ddeee2506cb4840072bcad41bf83842ded24fd56
Yogesh sahu <yogeshsahu15022004@gmail.com>
2025-08-03 11:56:00 +0530

    Add Project Files

--- app.js
+++ app.js
@@ -1,73 +1,89 @@
- if (process.env.NODE_ENV !== "production") {
-    require("dotenv").config();
+ if(process.env.NODE_ENV != "production"){
+    require('dotenv').config();
 }
+console.log(process.env.secret);
 
-const express = require("express");
-const app = express();
-const mongoose = require("mongoose");
-const path = require("path");
-const methodOverride = require("method-override");
-const ejsMate = require("ejs-mate");
-const ExpressError = require("./utils/ExpressError.js");
-const session = require("express-session");
-const MongoStore = require("connect-mongo");
-const flash = require("connect-flash");
-const passport = require("passport");
-const LocalStrategy = require("passport-local");
-const User = require("./models/user.js");
+const express = require("express"); // include express
+const app = express();  // all express thing inside the app
+const mongoose = require("mongoose");   // include mongoose for connect with node
 
-const listingRouter = require("./routes/listing.js");
-const reviewRouter = require("./routes/review.js");
-const userRouter = require("./routes/user.js");
+const dbUrl = process.env.ATLASDB_URL;   // build database connection Wonderlust is our database
+//const dbUrl = 'mongodb://127.0.0.1:27017/Wonderlust'
+const path = require('path'); //Include pata for views
+const methodOverride = require("method-override");  // for PUT request (when we edit listing)
+const ejsMate = require("ejs-mate");  // for creating boilerplates(layout)
+const ExpressError = require("./utils/ExpressError.js");    // custom ExpressErrors
+const session = require("express-session"); // using express-session (temprary memory storage inside cookie in development jurney)
+const MongoStore = require('connect-mongo');    //for mongo session store
+const flash = require("connect-flash"); // include for using flash messages
+const passport = require("passport");   // include passport
+const LocalStrategy = require("passport-local");    // require passport-local?
+const User = require("./models/user.js");   //  require user.js of signup schema
 
-const dbUrl = process.env.ATLASDB_URL;
+const listingRouter = require("./routes/listing.js");    // require listing routes
+const reviewRouter = require("./routes/review.js");      // require review routes
+const userRouter = require("./routes/user.js");     //  include user route
 
-main()
-    .then(() => {
-        console.log("Connected to DB");
+//error handling if occurs (when we make connection with database)
+main().
+    then(()=>{
+        console.log("connected to DB"); // connect successfully with db
+    })      
+    .catch((err)=>{
+        console.log(err);   // when occurs any problems with database connection 
     })
-    .catch((err) => {
-        console.log(err);
-    });
-
-async function main() {
-    await mongoose.connect(dbUrl);
+    
+// Database connection async function
+async function main(){
+   await mongoose.connect(dbUrl);    //dbUrl
 }
 
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
-app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride("_method"));
-app.engine("ejs", ejsMate);
-app.use(express.static(path.join(__dirname, "/public")));
 
+
+app.set("view engine","ejs");   // set view engine for templating
+app.set("views",path.join(__dirname,"views"));  //set so that we no need to include view its by deafult search templating inside view
+app.use(express.urlencoded({extended:true}));   //request data parsing
+app.use(methodOverride("_method")); // for PUT and Delete route
+app.engine('ejs', ejsMate); // templating or layouts 
+app.use(express.static(path.join(__dirname,"/public")));    // use for static file changes (html css etc.)
+
+// root rout (it execute when not found individual routs)
+// app.get("/",(req,res)=>{
+//     res.send("hi i'm root");
+// })
+
+// creating mongo session
 const store = MongoStore.create({
-    mongoUrl: dbUrl,
-    crypto: {
+    mongoUrl:dbUrl,
+    crypto:{
         secret: process.env.SECRET,
     },
-    touchAfter: 24 * 3600,
+    touchAfter: 24*3600,    // for 14 days
 });
 
-store.on("error", (err) => {
-    console.log("ERROR IN MONGO SESSION STORE", err);
+// for mongo store error detection
+store.on("error",()=>{
+    console.log("ERROR IN MONGO SESSION STORE",err)
 });
 
 const sessionOptions = {
     store,
     secret: process.env.SECRET,
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
-        maxAge: 1000 * 60 * 60 * 24 * 7,
-        httpOnly: true,
-    },
+    resave:false,
+    saveUninitialized:true,
+    cookie:{
+        expires: Date.now() + 7 * 24 * 60 * 60 * 1000,  // expire session in 1week
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        httpOnly:true,  // secure from Cross scripting attack
+    }
 };
 
-app.use(session(sessionOptions));
-app.use(flash());
 
+
+app.use(session(sessionOptions));   // express-session middleware
+app.use(flash());   // flash middleware for showing message once when new listings created
+
+// thease are for user authentication
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
@@ -75,28 +91,34 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use((req, res, next) => {
-    res.locals.success = req.flash("success");
-    res.locals.error = req.flash("error");
-    res.locals.currUser = req.user;
+// middleware for running flash
+app.use((req,res,next)=>{
+    res.locals.success = req.flash("success");  // success flash
+    res.locals.error = req.flash("error");  // error flash
+    res.locals.currUser = req.user; //current user information which currently logged in
     next();
-});
+})
 
-app.use("/listings", listingRouter);
-app.use("/listings/:id/reviews", reviewRouter);
-app.use("/", userRouter);
+// technique for using route
+app.use("/listings",listingRouter);      // ("/listings") is common in all listing route thats why we replace it with "/"
+app.use("/listings/:id/reviews",reviewRouter);   // ("/listings/:id/reviews") is common in all review routes thats why we replace it with "/"
+app.use("/",userRouter);    //  "/" is common for user user route
 
-app.all("*", (req, res, next) => {
-    console.log("404 for:", req.originalUrl);
-    next(new ExpressError(404, "Page not found"));
-});
+// // route for all requests which we dont matchs on my route
+app.all("*",(req,res,next)=>{
+    next(new ExpressError(404,"page not found!")); // throw custom ExpressError
+})
 
-// Error handler
-app.use((err, req, res, next) => {
-    const { statusCode = 500, message = "Something went wrong" } = err;
-    res.status(statusCode).render("error.ejs", { err });
-});
+//Error handling middleware
+app.use((err,req,res,next)=>{
+    let {status=500,message="SOME ISSUE"} = err;    // take status and message from err if not have that's why we set default 
+    //res.render("error.ejs",message);
+    res.status(status).render("error.ejs",{message});   //only messege send as a response status show only on page inspect and error show on error.ejs page
+})
 
-app.listen(8080, () => {
-    console.log("Server is listening on port 8080");
+
+
+// for running the nodejs server 
+app.listen(8080,()=>{
+    console.log("server is listing");
 });
